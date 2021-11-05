f125b1a5a44c9a7c311cc31efb3cddd1b506849d
Uros Randelovic <urandjelovic@microsoft.com>
2021-11-05 12:16:01 +0100

    - added more garbage terms

--- customerResearch/join_datasets.py
+++ customerResearch/join_datasets.py
@@ -29,13 +29,14 @@ def join_sets_on_subscription_id(how_to_join):
     return subs_from_kusto_enriched_with_dpdw_data
 
 
-def get_df_with_dropped_rows_matching_a_condition(dataframe, column_name, value_to_match):
-    print("Dropping rows where" + column_name + "contains" + value_to_match)
+def get_df_with_dropped_rows_matching_a_condition(dataframe, column_name, value_to_drop):
+    print("Dropping rows where" + column_name + "contains" + value_to_drop)
     dataframe[column_name] = dataframe[column_name].str.lower()
-    value_to_match = value_to_match.lower()
+
+    value_to_drop = value_to_drop.lower()
 
     # df_without_dropped_values = dataframe[dataframe[column_name].str.contains("|".join([value_to_match.lower()]))]
-    df_without_dropped_values = dataframe[~dataframe[column_name].str.contains('|'.join([value_to_match]), na=False)]
+    df_without_dropped_values = dataframe[~dataframe[column_name].str.contains('|'.join([value_to_drop]), na=False)]
 
     print('___________________________')
     print('df_without_dropped_values')
@@ -73,19 +74,120 @@ if __name__ == '__main__':
         "SalesExec",
         "ArcGis",
         "SqlQuery",
-        "SolarWinds"
+        "SolarWinds",
+        "DacFx",
+        "DataAccess",
+        "DatabaseMail",
+        "dbtools",
+        'dev-webhook',
+        'dw-etl',
+        'databasemail',
+        'sharepoint',
+        'azdata',
+        'sqleditor',
+        'python',
+        'node',
+        'php',
+        'apache',
+        'jobservice',
+        'loanspq',
+        'operating system',
+        'notification',
+        'mashup',
+        'microsoft office',
+        '/',
+        '-',
+        'msdb',
+        'master',
+        'tempdb',
+        'system.activities',
+        'defaultpool',
+        'localbuildtest',
+        'debugging',
+        'windowsapplication1',
+        'tedious',
+        'sqlcmd',
+        'websphere application server',
+        'vadmin',
+        'alteryx',
+        'lenosmodel',
+        'stdioprocessor',
+        'one identity',
+        'report server',
+        'replication monitor',
+        'discovery hub',
+        'jtds',
+        'dbupdate',
+        'meded',
+        'notifyhandler',
+        'tabprotosrv',
+        'visual studio',
+        'axdixf',
+        'gtkschedulereports',
+        'unit testing',
+        'ignitemonitor',
+        'report server',
+        'safeway .net framework',
+        'sql management',
+        'sendcryptokeytoserver',
+        'pearlabyss_blackdesert_real',
+        'apexsql source control',
+        'power automate desktop',
+        'supportpoint',
+        'laserfiche',
+        'mediapulse',
+        'snapshot',
+        'sqlsession',
+        'agent',
+        'sql server performance investigator',
+        'extract',
+        'replicat',
+        'persec',
+        'data',
+        'sync',
+        'foglight',
+        'linqpad',
+        'compliancemapenterprise',
+        'leadcontactassignmentaudit',
+
+    ]
+
+    useful_but_too_many = [
+        "campusnexus student",
+        "emerson",
+        'hankisoft.com',
+        'red gate',
+        'redgate',
+        'microsoft dynamics 365',
+        'archertech',
+        'dbforge',
+        'tibco',
+        'informatica',
+        'tableau',
+        'pearlabyss_blackdesert_real',
+
     ]
 
     joined_sets = join_sets_on_subscription_id(how_to_join='left')
 
+    # remove empty rows
+    joined_sets.dropna(subset=['ApplicationName'], inplace=True)
+
     dropped_internal_customers = get_df_with_dropped_rows_matching_a_condition(joined_sets,
                                                                                column_name="OrganizationName",
-                                                                               value_to_match="MICROSOFT")
-    dropped_junk_terms = dropped_internal_customers
+                                                                               value_to_drop="MICROSOFT")
+    dataset_without_specified_terms = dropped_internal_customers
 
     for junk_term in junk_terms:
-        dropped_junk_terms = get_df_with_dropped_rows_matching_a_condition(dropped_junk_terms,
-                                                                           column_name="ApplicationName",
-                                                                           value_to_match=junk_term)
+        dataset_without_specified_terms = get_df_with_dropped_rows_matching_a_condition(dataset_without_specified_terms,
+                                                                                        column_name="ApplicationName",
+                                                                                        value_to_drop=junk_term)
 
-    save_dataframe_to_disk(dropped_junk_terms, 'onlyValuableExternalCustomers.csv')
+    for useful_term in useful_but_too_many:
+        dataset_without_specified_terms = get_df_with_dropped_rows_matching_a_condition(dataset_without_specified_terms,
+                                                                                        column_name="ApplicationName",
+                                                                                        value_to_drop=useful_term)
+
+    dataset_without_specified_terms.sort_values(by="SubscriptionId", inplace=True)
+
+    save_dataframe_to_disk(dataset_without_specified_terms, 'onlyValuableExternalCustomers.csv')
